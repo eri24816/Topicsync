db0dbb7d1e5fbe6f2ed460b9acabdd19b035b981
secminhr <secminhrian@gmail.com>
2023-09-09 14:31:21 +0800

    multiple change in a transition

--- unittest/test_string_diff_undo_redo.py
+++ unittest/test_string_diff_undo_redo.py
@@ -1,6 +1,7 @@
 import unittest
 from chatroom.state_machine.state_machine import StateMachine
 from chatroom.topic import StringTopic
+from chatroom.change import StringChangeTypes
 
 class MyTestCase(unittest.TestCase):
     '''
@@ -40,4 +41,24 @@ class MyTestCase(unittest.TestCase):
 
         machine.redo(transitions[0])
         assert topic.get() == 'ae'
+        assert len(transitions) == 1
+
+    def test_multiple_change_in_transition(self):
+        transitions = []
+        machine = StateMachine(transition_callback=transitions.append)
+        topic = machine.add_topic('topic', StringTopic, init_value='abcde')
+
+        with machine.record():
+            topic.insert(2, 'xxx')
+            topic.delete(1, 'bxx')
+            topic.insert(3, 'yy')
+        assert topic.get() == 'axcyyde'
+        assert len(transitions) == 1
+
+        machine.undo(transitions[0])
+        assert topic.get() == 'abcde'
+        assert len(transitions) == 1
+
+        machine.redo(transitions[0])
+        assert topic.get() == 'axcyyde'
         assert len(transitions) == 1
